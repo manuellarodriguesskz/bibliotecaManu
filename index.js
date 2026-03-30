b0a3b34472892f01bc5cc53fcfc02e640401f431
MANUELLA RODRIGUES DA SILVA <manuella_silva150@estudante.sesisenai.org.br>
2026-03-30 10:44:51 -0300

    atualizado

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const routes = require('./src/routes/index.routes');
+const logger = require('./src/middlewares/logger.middlewares');
 const app = express();
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
+app.use(logger);
 
 // Utilizando as rotas
 app.use(routes);
